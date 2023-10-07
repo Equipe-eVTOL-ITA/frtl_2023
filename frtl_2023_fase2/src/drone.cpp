76f215df2b040e1e63c008f992ce4fdcd0f50572
veccon04 <vinicecconp@gmail.com>
2023-10-07 05:33:23 -0300

    estrutura da fase 2 safe

--- frtl_2023_fase2/src/drone.cpp
+++
@@ -1,4 +0,0 @@
-#include "fase2/drone.hpp"
-#include <rclcpp/rclcpp.hpp>
-#include <memory>
-#include <iostream>
---
+++ frtl_2023_fase2/src/fase2_safe.cpp
@@ -0,0 +1,8 @@
+#include "fase2_safe/takeoff_state.hpp"
+#include "fase2_safe/return_home_state.hpp"
+#include "fase2_safe/visit_base_state.hpp"
+#include "fase2_safe/landing_state.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+#include <memory>
+#include <iostream>
